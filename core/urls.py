4a43dbd9c3316c1b6469452e80d15027bcee5520
King29273 <ivantsov.artem2008@gmail.com>
2025-05-01 14:11:03 +0200

    Добавлен раздел новостей

--- core/urls.py
+++ core/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("user/", include("auth_sys.urls")),
     path('', include("forum.urls")),
+    path('news/', include('news.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
